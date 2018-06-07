45b98bbe64c73f16b9858b75f75bbf3e0dcf4d8d
Jason C <jforeza@gmail.com>
2018-06-07 11:39:07 -0700

    Initial project commit

---
+++ app/src/main/java/whereis/mypackage/ikeaordertracker/OrderTracker.java
@@ -0,0 +1,13 @@
+package whereis.mypackage.ikeaordertracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OrderTracker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_tracker);
+    }
+}
